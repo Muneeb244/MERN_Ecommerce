1ad665920e72ad4473d97f1c7b305266988147b0
Muneeb Ahmad <91079744+Muneeb244@users.noreply.github.com>
2024-01-18 11:39:47 +0500

    Initial commit

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Home from './pages/Home';
+import Search from './pages/Search';
+import Cart from './pages/Cart';
+
+const App = () => {
+  return <Router>
+    {/* Header */}
+    <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/search" element={<Search/>} />
+      <Route path="/cart" element={<Cart/>} />
+    </Routes>
+  </Router>
+}
+
+export default App
