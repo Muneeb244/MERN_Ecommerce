0811b39708c4ea80027b2ae596304012dc3a25ef
Muneeb Ahmad <91079744+Muneeb244@users.noreply.github.com>
2024-02-05 21:21:35 +0500

    completed cart and shipping page

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -2,9 +2,11 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { lazy, Suspense } from "react";
 import Loading from "./components/Loading";
 import Header from "./components/Header";
+
 const Home = lazy(() => import("./pages/Home"));
 const Search = lazy(() => import("./pages/Search"));
 const Cart = lazy(() => import("./pages/Cart"));
+const Shipping = lazy(() => import("./pages/Shipping"));
 
 // Admin Dashboard
 
@@ -31,35 +33,44 @@ const Stopwatch = lazy(() => import("./pages/admin/apps/Stopwatch"));
 const App = () => {
   return (
     <Router>
-      <Header/>
+      <Header />
       <Suspense fallback={<Loading />}>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/search" element={<Search />} />
           <Route path="/cart" element={<Cart />} />
 
+          {/* Logged in user routes */}
+
+          <Route>
+            <Route path="/shipping" element={<Shipping />} />
+          </Route>
+
           {/* Admin Dashboard */}
-          <Route path="/admin/dashboard" element={<Dashboard />} />
-          <Route path="/admin/products" element={<Products />} />
-          <Route path="/admin/transaction" element={<Transaction />} />
-          <Route path="/admin/customer" element={<Customer />} />
 
-          {/* charts */}
-          <Route path="/admin/chart/bar" element={<BarChart />} />
-          <Route path="/admin/chart/pie" element={<PieChart />} />
-          <Route path="/admin/chart/line" element={<LineChart />} />
+          <Route>
+            <Route path="/admin/dashboard" element={<Dashboard />} />
+            <Route path="/admin/products" element={<Products />} />
+            <Route path="/admin/transaction" element={<Transaction />} />
+            <Route path="/admin/customer" element={<Customer />} />
 
-          {/* management */}
-          <Route path="/admin/product/new" element={<NewProduct />} />
-          <Route path="/admin/product/:id" element={<ProductManagement />} />
-          <Route
-            path="/admin/transaction/:id"
-            element={<TransactionManagement />}
-          />
+            {/* charts */}
+            <Route path="/admin/chart/bar" element={<BarChart />} />
+            <Route path="/admin/chart/pie" element={<PieChart />} />
+            <Route path="/admin/chart/line" element={<LineChart />} />
 
-          {/* apps */}
-          <Route path="/admin/app/coupon" element={<Coupon />} />
-          <Route path="/admin/app/stopwatch" element={<Stopwatch />} />
+            {/* management */}
+            <Route path="/admin/product/new" element={<NewProduct />} />
+            <Route path="/admin/product/:id" element={<ProductManagement />} />
+            <Route
+              path="/admin/transaction/:id"
+              element={<TransactionManagement />}
+            />
+
+            {/* apps */}
+            <Route path="/admin/app/coupon" element={<Coupon />} />
+            <Route path="/admin/app/stopwatch" element={<Stopwatch />} />
+          </Route>
         </Routes>
       </Suspense>
     </Router>
--- Frontend/src/pages/Cart.tsx
+++ Frontend/src/pages/Cart.tsx
@@ -1,8 +1,86 @@
+import { useEffect, useState } from "react";
+import { VscError } from "react-icons/vsc";
+import CartItems from "../components/cartItems";
+import { Link } from "react-router-dom";
+
+const cartItems = [
+  {
+    productId: "afadsf",
+    photo:
+      "https://d1iv6qgcmtzm6l.cloudfront.net/products/lg_npXwHYuAuabxaCdxYw56ouORuEsgAhrczzoCmEOz.jpg",
+    name: "Macbook",
+    price: 3000,
+    quantity: 4,
+    stock: 10,
+  },
+];
+const subtotal = 4000;
+const tax = Math.round(subtotal * 0.8);
+const shippingCharges = 200;
+const discount = 300;
+const total = subtotal + shippingCharges + tax;
 
 const Cart = () => {
-  return (
-    <div>Cart</div>
-  )
-}
+  const [couponCode, setCouponCode] = useState<string>("");
+  const [isValidCouponCode, setIsValidCouponCode] = useState<boolean>(false);
 
-export default Cart
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      if (Math.random() > 0.5) setIsValidCouponCode(true);
+      else setIsValidCouponCode(false);
+    }, 1000);
+    return () => {
+      clearTimeout(timeoutId);
+      setIsValidCouponCode(false);
+    };
+  }, [couponCode]);
+
+  return (
+    <div className="cart">
+      <main>
+        { cartItems.length > 0 ? cartItems.map((i, index) => (
+          <CartItems key={index} cartItem={i} />
+        )) : <h1>No Items added</h1>
+        
+        }
+      </main>
+
+      <aside>
+        <p>Subtotal: {subtotal}rs</p>
+        <p>Shipping Charges:{shippingCharges}rs</p>
+        <p>Tax Charges: {tax}rs</p>
+        <p>
+          Discount: <em className="red"> - {discount}rs</em>
+        </p>
+        <p>
+          <b>Total: {total}rs</b>
+        </p>
+        <input
+          type="text"
+          value={couponCode}
+          onChange={(e) => setCouponCode(e.target.value)}
+          placeholder="Coupon code"
+        />
+
+        {couponCode &&
+          (isValidCouponCode ? (
+            <span className="green">
+              {discount}rs off using the
+              <code>{couponCode}</code>
+            </span>
+          ) : (
+            <span className="red">
+              Invalid Coupon code
+              <VscError />
+            </span>
+          ))}
+
+          {
+            cartItems.length > 0 && <Link to={"/shipping"}>Checkout</Link>
+          }
+      </aside>
+    </div>
+  );
+};
+
+export default Cart;
